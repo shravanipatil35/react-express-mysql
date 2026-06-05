684b4a32f250130e29a1d36bac4109ac9276930b
Shravani Patil <shravaniupatil4444@gmaiul.com>
2026-06-05 20:39:54 +0530

    fix: update CORS options to allow requests from any origin

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://shravani-react-frontend-2026-prod.s3-website.ap-south-1.amazonaws.com"
+  origin: "*"
 };
 
 app.use(cors(corsOptions));
